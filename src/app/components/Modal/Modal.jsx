bad9038968acef03cb1c53a1d081ca1c3d4f5a78
Colin Guinane <colinguinane@gmail.com>
2024-03-27 13:43:40 -0600

    adding dropdown menu to each album

--- src/app/components/Modal/Modal.jsx
+++ src/app/components/Modal/Modal.jsx
@@ -19,6 +19,12 @@ const Modal = ({
 }) => {
   const artist = "Colin Guinane";
   const [currentSongIndex, setCurrentSongIndex] = useState(-1);
+  const [dropdown, enableDropdown] = useState(false);
+
+  const dropdownList = () => {
+    enableDropdown(!dropdown); // Toggle the dropdown state
+    console.log(dropdown); // Output the current state (before the toggle)
+  };
 
   const handleClickOutside = (event) => {
     if (!event.target.closest(".modal-content")) {
@@ -47,67 +53,104 @@ const Modal = ({
         <span className="absolute right-0 p-2 cursor-pointer" onClick={onClose}>
           &times;
         </span>
-        <h2 className="font-bold mb-4 text-4xl text-white">{folderName}</h2>
+        <div className="flex">
+          <h2 className="font-bold mb-4 text-4xl text-white">{folderName}</h2>{" "}
+          <div
+            className="group border rounded-full max-h-fit mt-1 ml-5"
+            onClick={dropdownList}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-dots stroke-white cursor-pointer"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#2c3e50"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+              <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+              <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+            </svg>
+          </div>
+          {dropdown && (
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              className="flex -mt-1 mx-2"
+            >
+              {spotifyLink && (
+                <a href={spotifyLink} className="max-w-fit rounded-full mt-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-brand-spotify stroke-green-400 hover:scale-105 hover:stroke-green-200 active:scale-95 transition-all duration-300"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="#2c3e50"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                    <path d="M8 11.973c2.5 -1.473 5.5 -.973 7.5 .527" />
+                    <path d="M9 15c1.5 -1 4 -1 5 .5" />
+                    <path d="M7 9c2 -1 6 -2 10 .5" />
+                  </svg>
+                </a>
+              )}
+              {appleMusicLink && (
+                <a
+                  href={appleMusicLink}
+                  className="max-w-fit rounded-full mt-3"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-brand-apple stroke-pink-800"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="#2c3e50"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M8.286 7.008c-3.216 0 -4.286 3.23 -4.286 5.92c0 3.229 2.143 8.072 4.286 8.072c1.165 -.05 1.799 -.538 3.214 -.538c1.406 0 1.607 .538 3.214 .538s4.286 -3.229 4.286 -5.381c-.03 -.011 -2.649 -.434 -2.679 -3.23c-.02 -2.335 2.589 -3.179 2.679 -3.228c-1.096 -1.606 -3.162 -2.113 -3.75 -2.153c-1.535 -.12 -3.032 1.077 -3.75 1.077c-.729 0 -2.036 -1.077 -3.214 -1.077z" />
+                    <path d="M12 4a2 2 0 0 0 2 -2a2 2 0 0 0 -2 2" />
+                  </svg>
+                </a>
+              )}
+            </motion.div>
+          )}
+        </div>
         <h1 className="text-gray-300 font-extrabol">{yearReleased}</h1>
         <div className="flex">
           <h1 className="text-white max-w-[28rem] py-2">{albumDescription}</h1>
+        </div>
 
-          {spotifyLink && (
-            <a href={spotifyLink} className="max-w-fit rounded-full mt-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon icon-tabler icon-tabler-brand-spotify stroke-green-400 hover:scale-105 hover:stroke-green-200 active:scale-95 transition-all duration-300"
-                width="44"
-                height="44"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="#2c3e50"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-                <path d="M8 11.973c2.5 -1.473 5.5 -.973 7.5 .527" />
-                <path d="M9 15c1.5 -1 4 -1 5 .5" />
-                <path d="M7 9c2 -1 6 -2 10 .5" />
-              </svg>
-            </a>
+        <div className="border-b border-gray-500 pb-2 border-opacity-50">
+          {artistChoice && (
+            <h1 className="text-yellow-300 border-2 border-yellow-600 max-w-fit px-3 rounded-full my-2">
+              Artist's Choice:{" "}
+              <span className="font-extrabold">{artistChoice}</span>
+            </h1>
           )}
-          {appleMusicLink && (
-            <a href={appleMusicLink} className="max-w-fit rounded-full mt-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon icon-tabler icon-tabler-brand-apple stroke-pink-800"
-                width="44"
-                height="44"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="#2c3e50"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M8.286 7.008c-3.216 0 -4.286 3.23 -4.286 5.92c0 3.229 2.143 8.072 4.286 8.072c1.165 -.05 1.799 -.538 3.214 -.538c1.406 0 1.607 .538 3.214 .538s4.286 -3.229 4.286 -5.381c-.03 -.011 -2.649 -.434 -2.679 -3.23c-.02 -2.335 2.589 -3.179 2.679 -3.228c-1.096 -1.606 -3.162 -2.113 -3.75 -2.153c-1.535 -.12 -3.032 1.077 -3.75 1.077c-.729 0 -2.036 -1.077 -3.214 -1.077z" />
-                <path d="M12 4a2 2 0 0 0 2 -2a2 2 0 0 0 -2 2" />
-              </svg>
-            </a>
+          {websiteExclusive && (
+            <h1 className="text-green-400 border-2 border-green-600  max-w-fit px-3 rounded-full my-2">
+              <span className="font-extrabold">Website Exclusive</span>
+            </h1>
           )}
         </div>
-        {artistChoice && (
-          <h1 className="text-yellow-300 border-2 border-yellow-600  max-w-fit px-3 rounded-full my-2">
-            Artist's Choice:{" "}
-            <span className="font-extrabold">{artistChoice}</span>
-          </h1>
-        )}
-        {websiteExclusive && (
-          <h1 className="text-green-400 border-2 border-green-600  max-w-fit px-3 rounded-full my-2">
-            <span className="font-extrabold">Website Exclusive</span>
-          </h1>
-        )}
         <ul
-          className={`grid grid-cols-${
+          className={`pt-3 grid grid-cols-${
             folderContents.length > 5 ? "3" : "2"
           } gap-4`}
         >
