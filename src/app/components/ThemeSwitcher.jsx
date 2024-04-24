9247fcf0cca4b96a1c529ddda88ce4bbcfc95c82
Colin Guinane <colinguinane@gmail.com>
2024-04-24 15:47:09 -0600

    transition colors and added moon icon

--- src/app/components/ThemeSwitcher.jsx
+++ src/app/components/ThemeSwitcher.jsx
@@ -14,15 +14,14 @@ export function ThemeSwitcher() {
   return (
     <button
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className={`fixed left-20 top-2 items-center z-[100000] ${
+      className={`fixed left-20 top-[8.5px] items-center z-[100000] ${
         theme === "light" ? "bg-black text-white" : "bg-white text-black"
       } p-2 rounded-full`}
     >
-      {theme ===
-      (
+      {theme === "light" ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="icon icon-tabler icon-tabler-sun-high stroke-black"
+          class="icon icon-tabler icon-tabler-moon"
           width="22"
           height="22"
           viewBox="0 0 24 24"
@@ -33,18 +32,8 @@ export function ThemeSwitcher() {
           stroke-linejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M14.828 14.828a4 4 0 1 0 -5.656 -5.656a4 4 0 0 0 5.656 5.656z" />
-          <path d="M6.343 17.657l-1.414 1.414" />
-          <path d="M6.343 6.343l-1.414 -1.414" />
-          <path d="M17.657 6.343l1.414 -1.414" />
-          <path d="M17.657 17.657l1.414 1.414" />
-          <path d="M4 12h-2" />
-          <path d="M12 4v-2" />
-          <path d="M20 12h2" />
-          <path d="M12 20v2" />
+          <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
         </svg>
-      ) ? (
-        "Light"
       ) : (
         <svg
           xmlns="http://www.w3.org/2000/svg"
